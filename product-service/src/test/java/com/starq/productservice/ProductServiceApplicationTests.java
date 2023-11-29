414e4edb579ba4d37439c8876cfce56b91138155
greyhands2 <greyhands2@gmail.com>
2023-11-29 16:59:09 +0100

    added test container setup and wrote a test for getProductRequest

--- product-service/src/test/java/com/starq/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/starq/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,63 @@
 package com.starq.productservice;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.starq.productservice.dto.ProductRequest;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo"); 
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		//let's do an equivalent of JSON.stringify in java
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+		mockMvc.perform(post("/api/product")
+		.contentType(MediaType.APPLICATION_JSON)
+		.content(productRequestString))
+		.andExpect(status().isCreated());
+
+	}
+
+	private ProductRequest getProductRequest(){
+		return ProductRequest.builder()
+		.name("Google Pixel")
+		.description("Google's answer to Iphone")
+		.price(BigDecimal.valueOf(1100))
+		.build();
 	}
 
 }
